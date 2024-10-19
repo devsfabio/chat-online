4b3ca87e0267c78fcc5839a797f7617e60bffd76
devsfabio <fabiio403@gmail.com>
2024-10-18 21:13:44 -0300

    alteracao no servidor url

--- frontEnd/js/script.js
+++ frontEnd/js/script.js
@@ -78,7 +78,7 @@ const handleLogin = (event) => {
   login.style.display = 'none';
   chat.style.display = 'flex';
 
-  websocket = new WebSocket('ws://localhost:8080');
+  websocket = new WebSocket('wss://chat-backend-uw0y.onrender.com');
   websocket.onmessage = processMessage;
 };
 
